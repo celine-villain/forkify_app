d84918cae5851352261dd0e4cee745999d2f2ee1
Céline <celine.vlln@gmail.com>
2020-02-06 18:26:07 +0100

    Search functionnality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,36 @@
-// Global app controller
-import x from './test';
-const a = 23;
-console.log(`I imported ${x} from another module called test.js! Variable a is ${a}`);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
+/*
+Global state of the app :
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // Get query from the view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if (query) {
+        // new search object and add to state
+        state.search = new Search(query);
+
+        // prepare UI for results
+
+        // search for recipes
+        await state.search.getResults();
+
+        // render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
