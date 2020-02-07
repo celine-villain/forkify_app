260e3ce2e272bb173c4b627f5e76e1ecfd3b62b4
Céline <celine.vlln@gmail.com>
2020-02-07 18:18:55 +0100

    Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /*
 Global state of the app :
@@ -14,18 +14,21 @@ const state = {};
 const controlSearch = async () => {
     // Get query from the view
     const query = searchView.getInput();
-    console.log(query);
 
     if (query) {
         // new search object and add to state
         state.search = new Search(query);
 
         // prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // search for recipes
         await state.search.getResults();
 
         // render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
@@ -33,4 +36,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline')
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
